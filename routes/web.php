603a37d9afd1a50d907bc38031f32d31f03b0691
Mathias Grimm <mathiasgrimm@gmail.com>
2025-06-05 10:31:26 -0300

    wip

--- routes/web.php
+++ routes/web.php
@@ -12,11 +12,11 @@ Route::get('dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::get('/test1', function () {
-    return [
+    return json_encode([
         'ip' => request()->ip(),
         'ips' => request()->ips(),
         'headers' => request()->headers->all(),
-    ];
+    ], JSON_PRETTY_PRINT | JSON_UNESCAPED_LINE_TERMINATORS | JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
 });
 
 require __DIR__.'/settings.php';
